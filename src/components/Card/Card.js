39f97dc2aac624ed5dec834e69233017fb78c2bd
Justin V <justinvuser@gmail.com>
2018-09-27 10:25:47 -0400

    Adding to GitHub Pages

---
+++ src/components/Card/Card.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+
+  <div className="card">
+  <div className="card-body">
+    <h5 className="card-title"> {props.name}</h5>
+  
+     <img alt={props.id} src={props.image} onClick={() => props.clickTest(props.id)} className="clickTest" />
+     {/* <span onClick={() => props.clickTest(props.id)} className="clickTest">test</span> */}
+  </div>
+</div>
+
+    /* <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+        </ul>
+      </div>
+    </div>   */
+  );
+
+  export default Card;
